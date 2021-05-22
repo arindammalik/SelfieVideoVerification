50cfb16f144a27e4fe7bf26a2a71d2d52ca63b08
arindam.malik <arindam.malik@jeevansathi.com>
2021-05-22 05:33:07 +0530

    sh file updated

--- selfieVerification1.sh
+++ selfieVerification1.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
-#sh selfieVerification1.sh /home/lavesh/Downloads/user_video.webm /home/lavesh/Downloads/ankit.jpg
 source ~/env1/bin/activate
-cd /home/lavesh/Desktop/selfie/git/JsSmartMessageSuggestion/BlinkDetector
+cd /home/arindam/Desktop/selfie/git/SelfieVideoVerification/BlinkDetector
 output=$(python detect_blinks.py --shape-predictor shape_predictor_68_face_landmarks.dat --video $1)
 if [ $output -gt 1 ]
 then
-cd /home/lavesh/Desktop/selfie/git/JsSmartMessageSuggestion/face_recognition/face_recognition
+cd /home/arindam/Desktop/selfie/git/SelfieVideoVerification/face_recognition/face_recognition
 python face_recognition_cli.py --toCompare ../../BlinkDetector/suitableFrame/suitableFrameMin.jpeg --main ../../BlinkDetector/suitableFrame/suitableFrameMax.jpeg
 python face_recognition_cli.py --toCompare $2 --main ../../BlinkDetector/suitableFrame/suitableFrameMax.jpeg
 else
